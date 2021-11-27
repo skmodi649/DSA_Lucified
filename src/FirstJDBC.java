134b5ee51e8b9faed0bff972ae84f8cfa170775d
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-11-27 12:19:24 +0530

    First JDBC program commit

--- src/FirstJDBC.java
+++ src/FirstJDBC.java
@@ -1,10 +1,9 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class FirstJDBC {
     public static void main(String[] args) throws ClassNotFoundException {
+        System.out.println("Name : Suraj Kumar");
+        System.out.println("Registration Number : 20BCE2835");
         Connection con = null;
         try{
             // Loading the driver
@@ -16,11 +15,27 @@ public class FirstJDBC {
             System.out.println("Connection Estabslished!");
 
             Statement s=con.createStatement(); // insert a record into table
-            s.executeUpdate("INSERT INTO food_item_tabel VALUES(1006 , 'Faluda' , 179);");
+            s.executeUpdate("INSERT INTO food_item_tabel VALUES(1006 , 'Faluda' , 79);");
+            s.executeUpdate("INSERT INTO food_item_tabel VALUES(1007 , 'Kachauri' , 15);");
             System.out.println("Record Inserted!");
+            s.executeUpdate("DELETE from food_item_tabel where id=1001;");
+            System.out.println("Record deleted!");
+            s.executeUpdate("UPDATE food_item_tabel set price=101 WHERE id=1006;");
+            System.out.println("Price updated!");
+            ResultSet rs=s.executeQuery("SELECT * FROM food_item_tabel;");
+            //Fetching data from ResultSet and display
+            while (rs.next())
+            {
+                //to fetch value from a column having number type of value
+                int x = rs.getInt("id");
+                //to fetch value from a column having varchar/text type of value
+                String y = rs.getString("Name");
+                //to fetch value from a column having number type of value
+                int z = rs.getInt("Price");
+                System.out.println(x + "   " + y + " " + z);
+            }
             s.close();
             con.close();
-
         }catch(ClassNotFoundException e){
             System.out.println("Exception!"+e.getMessage());
         }
