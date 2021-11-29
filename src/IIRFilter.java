13e049a126f3397a5c9794d454cc9004aac043a9
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-11-29 09:55:53 +0530

    The algorithms/java main method commit

--- src/IIRFilter.java
+++ src/IIRFilter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class IIRFilter {
 
     private final int order;
@@ -85,6 +87,10 @@ public class IIRFilter {
 
     //Driver function
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of orders : ");
+        int order = sc.nextInt();
+        IIRFilter obj = new IIRFilter(order);
 
     }
 }
